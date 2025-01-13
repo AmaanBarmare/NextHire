c72d43e98966bce69a93e775865c39dd667172af
Amaan Barmare <136726206+AmaanBarmare@users.noreply.github.com>
2025-01-13 05:20:49 -0500

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -20,7 +20,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
 const corsOptions = {
-    origin:'https://nexthire-1qrr.onrender.com',
+    origin:'https://nexthire-kgbf.onrender.com',
     credentials:true
 }
 
